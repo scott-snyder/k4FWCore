476a5c08b808e8cda6566d17ae9b8dda94fc3d1b
sss <sss@karma>
2025-08-13 11:33:20 -0400

    Make some geometry methods const.
    Make const:
      - IGeoSvc::getDetector
      - IGeoSvc::constantAsString

--- k4Interface/include/k4Interface/IGeoSvc.h
+++ k4Interface/include/k4Interface/IGeoSvc.h
@@ -33,9 +33,9 @@ class GAUDI_API IGeoSvc : virtual public IService {
 public:
   DeclareInterfaceID(IGeoSvc, 1, 0);
   virtual dd4hep::DetElement getDD4HepGeo() = 0;
-  virtual dd4hep::Detector* getDetector() = 0;
+  virtual const dd4hep::Detector* getDetector() const = 0;
   virtual G4VUserDetectorConstruction* getGeant4Geo() = 0;
-  virtual std::string constantAsString(std::string const& name) = 0;
+  virtual std::string constantAsString(std::string const& name) const = 0;
 };
 
 #endif // IGEOSVC_H
