6968f167c185a93583d4947e13c71d9b62d079c3
sss <sss@karma>
2025-09-10 17:07:11 -0400

    Suppress deprecation warnings.

--- test/k4FWCoreTest/src/components/k4FWCoreTest_cellID_reader.cpp
+++ test/k4FWCoreTest/src/components/k4FWCoreTest_cellID_reader.cpp
@@ -16,6 +16,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
 #include "k4FWCoreTest_cellID_reader.h"
 #include "k4FWCoreTest_cellID_writer.h"
 
