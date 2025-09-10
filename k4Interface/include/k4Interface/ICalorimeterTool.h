9a3f54bc90527ea76fdd96e5059b1926e68175fa
sss <sss@karma>
2025-09-10 14:57:29 -0400

    Refresh of cellIDs

--- k4Interface/include/k4Interface/ICalorimeterTool.h
+++ k4Interface/include/k4Interface/ICalorimeterTool.h
@@ -39,8 +39,18 @@ class ICalorimeterTool : virtual public IAlgTool {
 public:
   DeclareInterfaceID(ICalorimeterTool, 1, 0);
 
-  virtual std::vector<uint64_t> cellIDs() const = 0;
+  /** Fill vector with all existing cells for this geometry.
+   */
+  virtual const std::vector<uint64_t>& cellIDs() const = 0;
+
+  /** Prepare a map of all existing cells in current geometry.
+   *   @param[out] aCells map of existing cells (and deposited energy, set to 0)
+   *   return Status code.
+   */
   virtual StatusCode prepareEmptyCells(std::unordered_map<uint64_t, double>& aCells) const = 0;
+
+  /** Return the segmentation associated with this geometry.
+   */
   virtual const dd4hep::DDSegmentation::Segmentation* segmentation() const = 0;
 };
 
