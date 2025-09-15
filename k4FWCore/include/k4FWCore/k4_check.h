6324990e37aacce233f501534207f3f97d994205
sss <sss@karma>
2025-09-15 14:31:20 -0400

    check2

--- k4FWCore/include/k4FWCore/k4_check.h
+++ k4FWCore/include/k4FWCore/k4_check.h
@@ -35,7 +35,39 @@
  * in something that evaluates to @c false (including @c StatusCode::FAILURE),
  * we emit an error message and immediately return a @c StatusCode::FAILURE.
  * This can be used to make checking the results of calls from Gaudi components
- * easier to read.
+ * easier to read.  For example, a typical call to something that returns
+ * a @c StatusCode might be implemented something like this:
+ *
+ *@code
+ *  StatusCode sc = something();
+ *  if (! sc.isSuccess() ) {
+ *    error() << "Something went wrong." << endmsg;
+ *    return sc;
+ *  }
+ @endcode
+ *
+ * This has several undesirable properties.  First, it is overly verbose.
+ * 80% of the code in this example is devoted to error handling; if there
+ * are many of these, it becomes difficult to read what the code is
+ * actually doing.
+ *
+ * Second, there is no standardization of the error messages produced,
+ * and they are sometimes difficult to trace back to the actual offending
+ * source code.
+ *
+ * Third, code like this typically gets written by cut-and-paste,
+ * which is notoriously error-prone.
+ *
+ * The helper here can address these issues; rather than the code above, use
+ *
+ *@code
+ *  K4_GAUDI_CHECK( something() );
+ @endcode
+ *
+ * The error message produced by this will contain the text of the
+ * expression that failed, as well as the corresponding source file and line.
+ * Note that macro has to be used in a context such as a Gaudi component
+ * where the @c error() function is defined to return a Gaudi @c MsgStream.
  */
 #define K4_GAUDI_CHECK(EXP)                                                                                            \
   do {                                                                                                                 \
