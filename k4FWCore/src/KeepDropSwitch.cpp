7be5095d3ea5494527d2bf8df0aa2990a49a803d
sss <sss@karma>
2025-12-11 16:23:32 -0500

    Suppress warnings.

--- k4FWCore/src/KeepDropSwitch.cpp
+++ k4FWCore/src/KeepDropSwitch.cpp
@@ -16,6 +16,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#if __GNUC__ >= 16
+#pragma GCC diagnostic ignored "-Warray-bounds"
+#endif
+
 #include "k4FWCore/KeepDropSwitch.h"
 
 #include <sstream>
