7ab9113ff3cea10decc4f467a9f0046cca687ac4
sss <sss@karma>
2026-02-19 05:16:09 -0500

    Refresh of indexer

--- k4Interface/include/k4Interface/ICaloIndexer.h
+++ k4Interface/include/k4Interface/ICaloIndexer.h
@@ -1,6 +1,5 @@
-// This file's extension implies that it's C, but it's really -*- C++ -*-.
 /*
- * Copyright (c) 2014-2026 Key4hep-Project.
+ * Copyright (c) 2014-2024 Key4hep-Project.
  *
  * This file is part of Key4hep.
  * See https://key4hep.github.io/key4hep-doc/ for further info.
@@ -24,15 +23,12 @@
  * @brief Interface for cell indexing.
  */
 
-
 #ifndef K4INTERFACE_ICALOINDEXER_H
 #define K4INTERFACE_ICALOINDEXER_H
 
-
+#include <cstdint>
 #include <memory>
 #include <span>
-#include <cstdint>
-
 
 /**
  * @brief Interface for cell indexing.
@@ -41,8 +37,7 @@
  * In particular, for a valid identifier @c id,
  * <code>cellIDs()[index(id)] == id</code>.
  */
-class ICaloIndexer
-{
+class ICaloIndexer {
 public:
   /// Index type.
   using index_t = uint32_t;
@@ -50,33 +45,28 @@ public:
 
   virtual ~ICaloIndexer() = default;
 
-
   /**
    * @brief Return the index of an identifier.
    * @param id The identifier to look for.
    *
    * Returns the index of @c id in @c cellIDs(), or @c INVALID.
    */
-  virtual index_t index (uint64_t id) const = 0;
-
+  virtual index_t index(uint64_t id) const = 0;
 
   /**
    * @brief Return the set of all identifiers that we index.
    */
   virtual std::span<const uint64_t> cellIDs() const = 0;
 
-
   /**
    * @brief Return the IDs of the detector(s) that we index.
    */
   virtual std::span<const int> detIDs() const = 0;
-
 
   /**
    * @brief Number of bits in the cell ID used for the detector ID.
    */
   virtual size_t detIDBits() const = 0;
 };
-
 
 #endif // not K4INTERFACE_ICALOINDEXER_H
